2f94b5099f8337a50f88116e317f819436da5968
Norris <Norris@srv.int>
2018-04-30 21:20:25 +0300

    fix

--- TestRefactoring/TestRefactoring/CreateTestFixtureCodeAction.cs
+++ TestRefactoring/TestRefactoring/CreateTestFixtureCodeAction.cs
@@ -9,85 +9,85 @@ using TestRefactoring.Extensions;
 
 namespace TestRefactoring
 {
-    /// <summary>
-    /// CodeAction for creating TestFixture for specified class
-    /// </summary>
-    public class CreateTestFixtureCodeAction : CodeActionWithOptions
-    {
-        private readonly Project _testProject;
-        private readonly Document _document;
-        private readonly TypeDeclarationSyntax _typeDecl;
-        private readonly SemanticModel _semanticModel;
-        private readonly Func<string, string, string, string, string> _codeGenerationFunction;
-        public override string Title { get; }
-
-
-        public CreateTestFixtureCodeAction(Project testProject, Document document, TypeDeclarationSyntax typeDecl, SemanticModel semanticModel, string title, Func<string, string, string, string, string> codeGenerationFunction)
+        /// <summary>
+        /// CodeAction for creating a TestFixture for a specified class
+        /// </summary>
+        public class CreateTestFixtureCodeAction : CodeActionWithOptions
         {
-            _testProject = testProject;
-            _document = document;
-            _typeDecl = typeDecl;
-            _semanticModel = semanticModel;
-            Title = title;
-            _codeGenerationFunction = codeGenerationFunction;
-        }
+            private readonly Project _testProject;
+            private readonly Document _document;
+            private readonly TypeDeclarationSyntax _typeDecl;
+            private readonly SemanticModel _semanticModel;
+            private readonly Func<string, string, string, string, string> _codeGenerationFunction;
+            public override string Title { get; }
 
-        public override object GetOptions(CancellationToken cancellationToken)
-        {
-            return new Object();
-        }
 
-        protected override Task<IEnumerable<CodeActionOperation>> ComputeOperationsAsync(object options, CancellationToken cancellationToken)
-        {
-            var addedDocument = CreateTextFixtureDocument(cancellationToken);
-
-            IEnumerable<CodeActionOperation> operations = new CodeActionOperation[]
+            public CreateTestFixtureCodeAction(Project testProject, Document document, TypeDeclarationSyntax typeDecl, SemanticModel semanticModel, string title, Func<string, string, string, string, string> codeGenerationFunction)
             {
-                new ApplyChangesOperation(addedDocument.Project.Solution),
-                new OpenDocumentOperation(addedDocument.Id, true)
-            };
+                _testProject = testProject;
+                _document = document;
+                _typeDecl = typeDecl;
+                _semanticModel = semanticModel;
+                Title = title;
+                _codeGenerationFunction = codeGenerationFunction;
+            }
 
-            return Task.FromResult(operations);
-        }
-
-        protected override async Task<IEnumerable<CodeActionOperation>> ComputePreviewOperationsAsync(CancellationToken cancellationToken)
-        {
-            var addedDocument = CreateTextFixtureDocument(cancellationToken);
-
-            IEnumerable<CodeActionOperation> operations = new CodeActionOperation[]
+            public override object GetOptions(CancellationToken cancellationToken)
             {
-                new ApplyChangesOperation(addedDocument.Project.Solution),
-            };
+                return new Object();
+            }
 
-            return await Task.FromResult(operations).ConfigureAwait(false);
-        }
+            protected override Task<IEnumerable<CodeActionOperation>> ComputeOperationsAsync(object options, CancellationToken cancellationToken)
+            {
+                var addedDocument = CreateTextFixtureDocument(cancellationToken);
+
+                IEnumerable<CodeActionOperation> operations = new CodeActionOperation[]
+                {
+                    new ApplyChangesOperation(addedDocument.Project.Solution),
+                    new OpenDocumentOperation(addedDocument.Id, true)
+                };
+
+                return Task.FromResult(operations);
+            }
+
+            protected override async Task<IEnumerable<CodeActionOperation>> ComputePreviewOperationsAsync(CancellationToken cancellationToken)
+            {
+                var addedDocument = CreateTextFixtureDocument(cancellationToken);
+
+                IEnumerable<CodeActionOperation> operations = new CodeActionOperation[]
+                {
+                    new ApplyChangesOperation(addedDocument.Project.Solution),
+                };
+
+                return await Task.FromResult(operations).ConfigureAwait(false);
+            }
 
   
-        private Document CreateTextFixtureDocument(CancellationToken cancellationToken)
-        {
-            // Get the symbol representing the type for which a test is being created
-            var typeSymbol = _semanticModel.GetDeclaredSymbol(_typeDecl, cancellationToken);
+            private Document CreateTextFixtureDocument(CancellationToken cancellationToken)
+            {
+                // Get the symbol representing the type for which a test is being created
+                var typeSymbol = _semanticModel.GetDeclaredSymbol(_typeDecl, cancellationToken);
 
 
-            // get the name and namepsace of current type 
-            var typeName = _typeDecl.Identifier.Text;
-            var typeNamespace = typeSymbol.GetContainingNamespace();
+                // get the name and namepsace of current type 
+                var typeName = _typeDecl.Identifier.Text;
+                var typeNamespace = typeSymbol.GetContainingNamespace();
 
-            // define name and namepsace for testfixture type
-            var testTypeName = typeName + "Tests";
-            var testTypeNamespace = _testProject.Name + "." + typeNamespace.Replace($"{_document.Project.Name}.", "");
+                // define name and namepsace for testfixture type
+                var testTypeName = typeName + "Tests";
+                var testTypeNamespace = _testProject.Name + "." + typeNamespace.Replace($"{_document.Project.Name}.", "");
 
-            // generate code with testfixture type declaration
-            var code = _codeGenerationFunction(typeName, typeNamespace, testTypeName, testTypeNamespace);
+                // generate code with testfixture type declaration
+                var code = _codeGenerationFunction(typeName, typeNamespace, testTypeName, testTypeNamespace);
 
-            // define filename and folders for testfixture file
-            var fileName = $"{typeName}Tests.cs";
-            var fileFolders = typeNamespace.Replace($"{_document.Project.Name}.", "").Split('.');
+                // define filename and folders for testfixture file
+                var fileName = $"{typeName}Tests.cs";
+                var fileFolders = typeNamespace.Replace($"{_document.Project.Name}.", "").Split('.');
 
-            // add textfixture declaration file to the integration test project
-            var addedDocument = _testProject.AddDocument(fileName, code, folders: fileFolders);
+                // add textfixture declaration file to the test project
+                var addedDocument = _testProject.AddDocument(fileName, code, folders: fileFolders);
 
-            return addedDocument;
-        }
+                return addedDocument;
+            }
     }
 }
